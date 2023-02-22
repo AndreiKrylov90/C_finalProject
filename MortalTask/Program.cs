3993ba725316d11ae287486d0705ad18f538ccfc
Andrei Krylov <dronovoy@yandex.ru>
2023-02-22 18:43:20 +0300

    start things were done and commented

--- MortalTask/Program.cs
+++ MortalTask/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача Финальная: Написать программу, которая из массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+int n = 10;
+Console.WriteLine($"В заданном массиве всего {n} элементов");
+
+string[] array = new string[n];
+array[0] = "dsvnk";
+array[1] = "hjk";
+array[2] = "hglhll";
+array[3] = "hg6";
+array[4] = "jlk4fj";
+array[5] = "glhgl";
+array[6] = "123";
+array[7] = "679jgj";
+array[8] = "hu";
+array[9] = "j";
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.WriteLine($"Внимание, ваш массив:");
+PrintArray(array);
+
